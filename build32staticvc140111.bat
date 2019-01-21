e1702bbe6d3cbf827f23bacf6700034c7ab79dc8
Jan-E <github@ehrhardt.nl>
2019-01-21 02:15:31 +0100

    Enable SASLDB static (using libdb48s.lib) in batch-files

--- build32staticvc140111.bat
+++ build32staticvc140111.bat
@@ -5,7 +5,7 @@ if exist c:\cmu rd c:\cmu /s /q
 set old_include=%include%
 set include=%include%
 copy \php-sdk\win32build\lib\libsqlite3_a.lib \php-sdk\win32build\lib\libsqlite3.lib /y
-nmake /F NTMakefile STATIC_PLUGIN=yes SQL=SQLITE3 SQLITE_INCLUDES3=\php-sdk\win32build\include SQLITE_LIBPATH3=\php-sdk\win32build\lib LDAP=1 LDAP_INCLUDE=\php-sdk\win32build\include\openldap LDAP_LIB_BASE=\php-sdk\winlibs\openldap-2.4.47-noSASL\win32build.vc14o111noSASL\lib SASLDB=0 OPENSSL_INCLUDE=\php-sdk\win32build\include OPENSSL_LIBPATH=\php-sdk\win32build\lib NTLM=1 SRP=0 OTP=1 install
+nmake /F NTMakefile STATIC_PLUGIN=yes SQL=SQLITE3 SQLITE_INCLUDES3=\php-sdk\win32build\include SQLITE_LIBPATH3=\php-sdk\win32build\lib LDAP=1 LDAP_INCLUDE=\php-sdk\win32build\include\openldap LDAP_LIB_BASE=\php-sdk\winlibs\openldap-2.4.47-noSASL\win32build.vc14o111noSASL\lib SASLDB=1 DBLIB=berkeley DB_LIB=\php-sdk\win32build\lib\libdb48s.lib DB_INCLUDE=\php-sdk\win32build\include OPENSSL_INCLUDE=\php-sdk\win32build\include OPENSSL_LIBPATH=\php-sdk\win32build\lib NTLM=1 SRP=0 OTP=1 install
 set include=%old_include%
 xcopy plugins\sasl*.pdb c:\cmu\bin\sasl2\ /y
 dir *.dll /s
--- build64static.bat
+++ build64static.bat
@@ -5,7 +5,7 @@ if exist c:\cmu rd c:\cmu /s /q
 set old_include=%include%
 set include=%include%;\php-sdk\winlibs\cyrus-sasl\win32\include;\php-sdk\winlibs\cyrus-sasl\include
 copy \php-sdk\win64build\lib\libsqlite3_a.lib \php-sdk\win64build\lib\libsqlite3.lib /y
-nmake /F NTMakefile STATIC_PLUGIN=yes SQL=SQLITE3 SQLITE_INCLUDES3=\php-sdk\win64build\include SQLITE_LIBPATH3=\php-sdk\win64build\lib LDAP=1 LDAP_INCLUDE=\php-sdk\win64build\include\openldap LDAP_LIB_BASE=\php-sdk\winlibs\openldap-2.4.47-noSASL\win64build.vc15noSASL\lib SASLDB=0 OPENSSL_INCLUDE=\php-sdk\win64build\include OPENSSL_LIBPATH=\php-sdk\win64build\lib NTLM=1 SRP=0 OTP=1 install
+nmake /F NTMakefile STATIC_PLUGIN=yes SQL=SQLITE3 SQLITE_INCLUDES3=\php-sdk\win64build\include SQLITE_LIBPATH3=\php-sdk\win64build\lib LDAP=1 LDAP_INCLUDE=\php-sdk\win64build\include\openldap LDAP_LIB_BASE=\php-sdk\winlibs\openldap-2.4.47-noSASL\win64build.vc15noSASL\lib SASLDB=1 DBLIB=berkeley DB_LIB=\php-sdk\win64build\lib\libdb48s.lib DB_INCLUDE=\php-sdk\win64build\include OPENSSL_INCLUDE=\php-sdk\win64build\include OPENSSL_LIBPATH=\php-sdk\win64build\lib NTLM=1 SRP=0 OTP=1 install
 set include=%old_include%
 xcopy plugins\sasl*.pdb c:\cmu\bin\sasl2\ /y
 dir *.dll /s
