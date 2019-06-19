35a8348812bac97f25d0d0f01722df20003d67de
Jan-E <github@ehrhardt.nl>
2019-06-19 11:10:57 +0200

    Copy *.exe to c:\cmu\bin

--- build64static.bat
+++ build64static.bat
@@ -8,6 +8,8 @@ copy \php-sdk\win64build\lib\libsqlite3_a.lib \php-sdk\win64build\lib\libsqlite3
 nmake /F NTMakefile STATIC_PLUGIN=yes SQL=SQLITE3 SQLITE_INCLUDES3=\php-sdk\win64build\include SQLITE_LIBPATH3=\php-sdk\win64build\lib LDAP=1 LDAP_INCLUDE=\php-sdk\win64build\include\openldap LDAP_LIB_BASE=\php-sdk\winlibs\openldap-2.4.47-noSASL\win64build.vc15noSASL\lib SASLDB=1 DBLIB=berkeley DB_LIB=\php-sdk\win64build\lib\libdb48s.lib DB_INCLUDE=\php-sdk\win64build\include OPENSSL_INCLUDE=\php-sdk\win64build\include OPENSSL_LIBPATH=\php-sdk\win64build\lib NTLM=1 SRP=0 OTP=1 install
 set include=%old_include%
 xcopy plugins\sasl*.pdb c:\cmu\bin\sasl2\ /y
+xcopy sample\*.exe c:\cmu\bin\ /y
+xcopy utils\*.exe c:\cmu\bin\ /y
 dir *.dll /s
 dir *.exe /s
 
